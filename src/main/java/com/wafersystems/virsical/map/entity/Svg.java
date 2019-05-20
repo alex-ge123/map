b0b5e01b2dd8b2f8d713128d2eb801b689547168
tandongkui <tandongkui@wafersystems.com>
2019-05-20 18:32:00 +0800

    处理代码格式化；

--- src/main/java/com/wafersystems/virsical/map/entity/Svg.java
+++ src/main/java/com/wafersystems/virsical/map/entity/Svg.java
@@ -1,21 +1,18 @@
 package com.wafersystems.virsical.map.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
-
-import java.time.LocalDateTime;
-
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
-
-import java.io.Serializable;
-
+import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
