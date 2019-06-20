ca7b4062bb2c782147f3be70eca3ddcab202f000
tandongkui <tandongkui@wafersystems.com>
2019-06-20 16:29:28 +0800

    调整接口返回提示码；

--- src/main/java/com/wafersystems/virsical/map/common/MsgConstants.java
+++ src/main/java/com/wafersystems/virsical/map/common/MsgConstants.java
@@ -10,30 +10,30 @@ public interface MsgConstants {
   /**
    * 同一楼下楼层号不能重复
    */
-  String FLOOR_NUM_NO_REPEAT = "2001";
+  String FLOOR_NUM_NO_REPEAT = "104001";
 
   /**
    * 同一素材下素材状态标识不能重复
    */
-  String SVG_STATE_NO_REPEAT = "2002";
+  String SVG_STATE_NO_REPEAT = "104002";
 
   /**
    * SVG文件不能为空
    */
-  String SVG_FILE_NO_NULL = "2003";
+  String SVG_FILE_NO_NULL = "104003";
 
   /**
    * SVG文件解析异常
    */
-  String SVG_FILE_PARSE_EXCEPTION = "2004";
+  String SVG_FILE_PARSE_EXCEPTION = "104004";
 
   /**
    * 地图元素不能为空
    */
-  String MAP_ELEMENT_NO_NULL = "2005";
+  String MAP_ELEMENT_NO_NULL = "104005";
 
   /**
    * 已存在此园区名称
    */
-  String PARK_NAME_NO_REPEAT = "2006";
+  String PARK_NAME_NO_REPEAT = "104006";
 }
