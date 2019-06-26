d63f33da39f0c4cd81fc149b459839176b4f5d39
tandongkui <tandongkui@wafersystems.com>
2019-06-26 13:48:27 +0800

    常量定义转移到class；

--- src/main/java/com/wafersystems/virsical/map/common/MapConstants.java
+++ src/main/java/com/wafersystems/virsical/map/common/MapConstants.java
@@ -4,24 +4,24 @@ package com.wafersystems.virsical.map.common;
  * @author tandk
  * @date 2019/4/30 10:13
  */
-public interface MapConstants {
+public final class MapConstants {
   /**
    * 默认地图
    */
-  Integer MAP_TYPE_DEFAULT = 0;
+  public static final Integer MAP_TYPE_DEFAULT = 0;
 
   /**
    * 蜂鸟地图
    */
-  Integer MAP_TYPE_FENG_MAP = 1;
+  public static final Integer MAP_TYPE_FENG_MAP = 1;
 
   /**
    * 启用
    */
-  Integer OPEN_STATE = 0;
+  public static final Integer OPEN_STATE = 0;
 
   /**
    * 关闭
    */
-  Integer CLOSE_STATE = 1;
+  public static final Integer CLOSE_STATE = 1;
 }
--- src/main/java/com/wafersystems/virsical/map/common/MapMsgConstants.java
+++ src/main/java/com/wafersystems/virsical/map/common/MapMsgConstants.java
@@ -19,59 +19,59 @@ package com.wafersystems.virsical.map.common;
  * @author tandk
  * @date 2019/4/30 10:13
  */
-public interface MapMsgConstants {
+public final class MapMsgConstants {
   /**
    * 同一楼下楼层号不能重复
    */
-  String FLOOR_NUM_NO_REPEAT = "104001";
+  public static final String FLOOR_NUM_NO_REPEAT = "104001";
 
   /**
    * 同一素材下素材状态标识不能重复
    */
-  String SVG_STATE_NO_REPEAT = "104002";
+  public static final String SVG_STATE_NO_REPEAT = "104002";
 
   /**
    * SVG文件不能为空
    */
-  String SVG_FILE_NO_NULL = "104003";
+  public static final String SVG_FILE_NO_NULL = "104003";
 
   /**
    * SVG文件解析异常
    */
-  String SVG_FILE_PARSE_EXCEPTION = "104004";
+  public static final String SVG_FILE_PARSE_EXCEPTION = "104004";
 
   /**
    * 地图元素不能为空
    */
-  String MAP_ELEMENT_NO_NULL = "104005";
+  public static final String MAP_ELEMENT_NO_NULL = "104005";
 
   /**
    * 已存在此园区名称
    */
-  String PARK_NAME_NO_REPEAT = "104006";
+  public static final String PARK_NAME_NO_REPEAT = "104006";
 
   /**
    * 当前园区下，楼宇名称已存在
    */
-  String BUILDING_HAS_EXIST = "104007";
+  public static final String BUILDING_HAS_EXIST = "104007";
 
   /**
    * 一体机编号不能重复
    */
-  String BUILDING_TOKEN_HAS_EXIST = "104008";
+  public static final String BUILDING_TOKEN_HAS_EXIST = "104008";
 
   /**
    * 当前园区存在楼宇，请先删除楼宇
    */
-  String THIS_PARK_HAS_BUILDING = "104009";
+  public static final String THIS_PARK_HAS_BUILDING = "104009";
 
   /**
    * 当前楼宇存在楼层，请先删除楼层
    */
-  String THIS_BUILDING_HAS_FLOOR = "104010";
+  public static final String THIS_BUILDING_HAS_FLOOR = "104010";
 
   /**
    * 当前楼层存在对应地图，请先删除地图
    */
-  String THIS_FLOOR_HAS_MAP = "104011";
+  public static final String THIS_FLOOR_HAS_MAP = "104011";
 }
