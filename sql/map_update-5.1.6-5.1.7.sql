c2e58ea1ab3f321f621b0992a770e20c030a3a26
tandongkui <tandongkui@wafersystems.com>
2020-08-11 16:19:12 +0800

    Changed 修改数据库升级脚本

--- sql/map_update-5.1.6-5.1.7.sql
+++ sql/map_update-5.1.6-5.1.7.sql
@@ -27,7 +27,7 @@ alter table `map_element` modify COLUMN `map_web_id` varchar(50) CHARACTER SET u
 -- ----------------------------
 ALTER TABLE `map` ADD `cmap_project_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图项目ID';
 ALTER TABLE `map` ADD `cmap_building_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图建筑ID';
-ALTER TABLE `map` ADD `cmap_floor_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图楼层ID',
+ALTER TABLE `map` ADD `cmap_floor_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图楼层ID';
 ALTER TABLE `map` ADD `cmap_cloud_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图云ID';
 ALTER TABLE `map` ADD `cmap_accesstoken` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图提供的验证key';
 ALTER TABLE `map` ADD `cmap_server` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创互地图接口服务地址';
