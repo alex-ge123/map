68b119520c3025115d8058149a167ee8a8b296a7
tandongkui <tandongkui@wafersystems.com>
2021-03-23 10:18:06 +0800

    Changed 删掉原升级脚本

--- sql/map_update-5.1.4-5.15.sql
+++
@@ -1,3 +0,0 @@
-USE `virsical_map`;
-
-INSERT INTO `svg_type` VALUES ('locker', '储物柜', 0, '2019-05-08 00:00:00', NULL);
--- sql/update-v5.1.3.100.sql
+++
@@ -1,3 +0,0 @@
-USE `virsical_map`;
-
-alter table svg add direction smallint(1) NOT NULL DEFAULT 0 COMMENT '文字方向';
